51d38fac67700b22fca8b3407983935b69e01747
Jesse Hallett <jesse.hallett@originate.com>
2020-12-27 13:25:49 -0500

    fix: `getTestDatabase` must set `DATABASE_URL` before running TypeORM migrations

--- src/getTestDatabase.ts
+++ src/getTestDatabase.ts
@@ -5,12 +5,11 @@ import {
 import { createConnection } from "typeorm";
 
 /**
- * Spins up a postgres database in a temporary Docker container. Returns an
- * object with a connection URI for that database, and a function to call to stop
- * and remove the container.
+ * Spins up a postgres database in a temporary Docker container. Sets the
+ * environment variable, `DATABASE_URL`. Returns an object with a connection URI
+ * for that database, and a function to call to stop and remove the container.
  */
 export async function getTestDatabase(): Promise<{
-  databaseUrl: string;
   stop: () => Promise<void>;
 }> {
   const config: Config = {
@@ -21,11 +20,11 @@ export async function getTestDatabase(): Promise<{
     ensureShutdown: true,
   };
   const { port, stop } = await startPostgresContainer(config);
-  const databaseUrl = `postgres://${config.user}:${config.password}@localhost:${port}/${config.database}`;
+  process.env.DATABASE_URL = `postgres://${config.user}:${config.password}@localhost:${port}/${config.database}`;
 
   const conn = await createConnection();
   await conn.runMigrations();
   await conn.close();
 
-  return { databaseUrl, stop };
+  return { stop };
 }
