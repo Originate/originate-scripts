2cb70e63fad90e5109b92bd509ef2ab513a793a1
Jesse Hallett <jesse@sitr.us>
2021-04-08 17:48:59 -0400

    feat: update `gql` template tag to permit interpolations (#2)

--- src/gql.ts
+++ src/gql.ts
@@ -2,6 +2,11 @@
  * gql template tag that just returns the query as a string for use in backend
  * tests
  */
-export function gql(query: TemplateStringsArray): string {
-  return query.join("\n");
+export function gql(
+  query: TemplateStringsArray,
+  ...interpolations: string[]
+): string {
+  return query
+    .flatMap((string, index) => [string, interpolations[index]])
+    .join("");
 }
